59ad4d50cac58f52b84546405682ed11f832d27b
Trent Doering <trent_doering@outlook.com>
2020-02-13 19:35:40 -0600

    .NET Core Simulated Dice Rolling Library

---
+++ Die.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimulatedDiceRolling
+{
+    public class Die
+    {
+        public Die(int sides)
+        {
+            Sides = sides;
+        }
+
+        public int Sides 
+        { 
+            get;
+            private set; 
+        }
+    }
+}
