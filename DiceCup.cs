8892f072d6ea820f4e386694041993811bf11e7d
Trent Doering <trent_doering@outlook.com>
2020-02-15 12:43:46 -0600

    Added TestApp project and DiceCup class

---
+++ DiceCup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulatedDiceRolling
+{
+    public class DiceCup : List<Die>
+    {
+        private Random randomNumberGenerator = new Random();
+        public int LastDiceRollTotal;
+
+        public DiceCup()
+        {
+            LastDiceRollTotal = 0;
+        }
+
+        public void Shake(int randomNumberSeed)
+        {
+            randomNumberGenerator = new Random(randomNumberSeed);
+        }
+
+        public int[] Roll()
+        {
+            LastDiceRollTotal = 0;
+            int[] dieRollResults = new int[this.Count];
+            for (int dieNumber = 0; dieNumber < dieRollResults.Length; ++dieNumber)
+            {
+                dieRollResults[dieNumber] = randomNumberGenerator.Next(1, this[dieNumber].Sides);
+                LastDiceRollTotal += dieRollResults[dieNumber];
+            }
+            return dieRollResults;
+        }
+    }
+}
