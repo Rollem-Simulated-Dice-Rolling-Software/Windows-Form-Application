1f1c7cf41e000d0b535e363a82bc54cb6b9966b9
Trent Doering <trent_doering@outlook.com>
2020-02-21 19:25:03 -0600

    Added Windows Form .NET core application

--- TestWindowsFormsCoreApp/DiceTestForm.Designer.cs
+++ TestWindowsFormsCoreApp/DiceTestForm.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace WindowsFormDiceTestApp
+﻿namespace TestWindowsFormsCoreApp
 {
     partial class DiceTestForm
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,15 +23,15 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.openGLControl = new OpenTK.GLControl();
             this.SuspendLayout();
             // 
-            // openGLControl
+            // glControl1
             // 
             this.openGLControl.BackColor = System.Drawing.Color.Black;
             this.openGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -41,23 +41,24 @@
             this.openGLControl.Size = new System.Drawing.Size(1200, 692);
             this.openGLControl.TabIndex = 0;
             this.openGLControl.VSync = false;
+
+            this.components = new System.ComponentModel.Container();
+
             // 
             // DiceTestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.openGLControl);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "DiceTestForm";
             this.Text = "Dice Test";
-            this.ResumeLayout(false);
-
         }
 
-        #endregion
-
         private OpenTK.GLControl openGLControl;
+
+        #endregion
     }
 }
 
---
+++ TestWindowsFormsCoreApp/DiceTestForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWindowsFormsCoreApp
+{
+    public partial class DiceTestForm : Form
+    {
+        public DiceTestForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
