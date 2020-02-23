1eb94b2f8e4332fb7dd528abaad5a74eb68d0ae8
Trent Doering <trent_doering@outlook.com>
2020-02-23 14:10:01 -0600

    Cleanup

--- TestWindowsFormsCoreApp/DiceTestForm.Designer.cs
+++ TestWindowsFormsCoreApp/DiceTestForm.Designer.cs
@@ -28,19 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.openGLControl = new OpenTK.GLControl();
             this.SuspendLayout();
-            // 
-            // glControl1
-            // 
-            this.openGLControl.BackColor = System.Drawing.Color.Black;
-            this.openGLControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.openGLControl.Location = new System.Drawing.Point(0, 0);
-            this.openGLControl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.openGLControl.Name = "openGLControl";
-            this.openGLControl.Size = new System.Drawing.Size(1200, 692);
-            this.openGLControl.TabIndex = 0;
-            this.openGLControl.VSync = false;
 
             this.components = new System.ComponentModel.Container();
 
@@ -50,13 +38,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.openGLControl);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "DiceTestForm";
             this.Text = "Dice Test";
         }
-
-        private OpenTK.GLControl openGLControl;
 
         #endregion
     }
