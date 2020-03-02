febf7b2679a2aa7d4263ddea9d9fb674207f3488
Trent Doering <trent_doering@outlook.com>
2020-03-01 20:34:38 -0600

    Adding new resources to dice cup control

--- WindowsFormsDiceTestAppFrameworks/View/DiceCupControl.Designer.cs
+++ WindowsFormsDiceTestAppFrameworks/View/DiceCupControl.Designer.cs
@@ -28,18 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceCupControl));
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Four", 0);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Six", 1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Eight", 2);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Ten", 3);
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Twelve", 4);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("Twenty", 5);
+            this.listBoxDiceCupContent = new System.Windows.Forms.ListBox();
+            this.imageListDiceSmall = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.listViewDiceToChooseFrom = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
             this.SuspendLayout();
             // 
-            // DiceCupView
+            // listBoxDiceCupContent
+            // 
+            this.listBoxDiceCupContent.AllowDrop = true;
+            this.listBoxDiceCupContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxDiceCupContent.FormattingEnabled = true;
+            this.listBoxDiceCupContent.ItemHeight = 25;
+            this.listBoxDiceCupContent.Location = new System.Drawing.Point(0, 0);
+            this.listBoxDiceCupContent.Name = "listBoxDiceCupContent";
+            this.listBoxDiceCupContent.Size = new System.Drawing.Size(459, 186);
+            this.listBoxDiceCupContent.TabIndex = 0;
+            this.listBoxDiceCupContent.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxDiceCupContent_DragDrop);
+            this.listBoxDiceCupContent.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxDiceCupContent_DragEnter);
+            this.listBoxDiceCupContent.DragOver += new System.Windows.Forms.DragEventHandler(this.listBoxDiceCupContent_DragOver);
+            // 
+            // imageListDiceSmall
+            // 
+            this.imageListDiceSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListDiceSmall.ImageStream")));
+            this.imageListDiceSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListDiceSmall.Images.SetKeyName(0, "dice-four-faces.png");
+            this.imageListDiceSmall.Images.SetKeyName(1, "perspective-dice-five.png");
+            this.imageListDiceSmall.Images.SetKeyName(2, "dice-eight-faces-eight.png");
+            this.imageListDiceSmall.Images.SetKeyName(3, "dice-ten-faces_zero.png");
+            this.imageListDiceSmall.Images.SetKeyName(4, "dice-twelve-faces_twelve.png");
+            this.imageListDiceSmall.Images.SetKeyName(5, "dice-twenty-faces-one.png");
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.listViewDiceToChooseFrom);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.listBoxDiceCupContent);
+            this.splitContainer.Size = new System.Drawing.Size(459, 484);
+            this.splitContainer.SplitterDistance = 294;
+            this.splitContainer.TabIndex = 1;
+            // 
+            // listViewDiceToChooseFrom
+            // 
+            this.listViewDiceToChooseFrom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewDiceToChooseFrom.HideSelection = false;
+            this.listViewDiceToChooseFrom.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.listViewDiceToChooseFrom.LargeImageList = this.imageListDiceSmall;
+            this.listViewDiceToChooseFrom.Location = new System.Drawing.Point(0, 0);
+            this.listViewDiceToChooseFrom.Name = "listViewDiceToChooseFrom";
+            this.listViewDiceToChooseFrom.Size = new System.Drawing.Size(459, 294);
+            this.listViewDiceToChooseFrom.SmallImageList = this.imageListDiceSmall;
+            this.listViewDiceToChooseFrom.TabIndex = 0;
+            this.listViewDiceToChooseFrom.UseCompatibleStateImageBehavior = false;
+            this.listViewDiceToChooseFrom.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.listViewDiceToChooseFrom_ItemDrag);
+            this.listViewDiceToChooseFrom.DragEnter += new System.Windows.Forms.DragEventHandler(this.listViewDiceToChooseFrom_DragEnter);
+            // 
+            // DiceCupControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "DiceCupView";
+            this.Controls.Add(this.splitContainer);
+            this.Name = "DiceCupControl";
             this.Size = new System.Drawing.Size(459, 484);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxDiceCupContent;
+        private System.Windows.Forms.ImageList imageListDiceSmall;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.ListView listViewDiceToChooseFrom;
     }
 }
--- WindowsFormsDiceTestAppFrameworks/View/DiceCupControl.cs
+++ WindowsFormsDiceTestAppFrameworks/View/DiceCupControl.cs
@@ -1,20 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsDiceTestAppFrameworks.Model;
+using System.Collections;
 
 namespace WindowsFormsDiceTestAppFrameworks.View
 {
-    public partial class DiceCupControl : UserControl
+    public partial class DiceCupControl : UserControl, IList<Die>
     {
+        // Description of Function
+        // ListView contains a collection of images that can be drag and dropped into the ListBox
+        // Drag and Drop operation should include:
+        // OnDrag - Save current cursor
+        // OnDragEnter and OnDragOver - Mouse cursor turns into dice that's being dragged
+        // OnLeave - Mouse cursor turns into saved current cursor
+        // OnDragDrop - Add dice to dice cup content control
+        // Add button to refill dice cup with previous dice
+        // Add button to clear dice cup, requiring new fill operations
         public DiceCupControl()
         {
             InitializeComponent();
+        }
+
+        public Die this[int index] 
+        {
+            get
+            {
+                return listBoxDiceCupContent.Items[index] as Die;
+            }
+            set
+            {
+                listBoxDiceCupContent.Items[index] = value;
+            }
+        }
+
+        public int Count { get { return listBoxDiceCupContent.Items.Count; } }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public void Add(Die item)
+        {
+            listBoxDiceCupContent.Items.Add(item);
+        }
+
+        public void Clear()
+        {
+            listBoxDiceCupContent.Items.Clear();
+        }
+
+        public bool Contains(Die item)
+        {
+            return listBoxDiceCupContent.Items.Contains(item);
+        }
+
+        public void CopyTo(Die[] array, int arrayIndex)
+        {
+            listBoxDiceCupContent.Items.CopyTo(array as Die[], arrayIndex);
+        }
+
+        public IEnumerator<Die> GetEnumerator()
+        {
+            return listBoxDiceCupContent.Items.GetEnumerator() as IEnumerator<Die>;
+        }
+
+        public int IndexOf(Die item)
+        {
+            return listBoxDiceCupContent.Items.IndexOf(item);
+        }
+
+        public void Insert(int index, Die item)
+        {
+            listBoxDiceCupContent.Items.Insert(index, item);
+        }
+
+        public bool Remove(Die item)
+        {
+            listBoxDiceCupContent.Items.Remove(item);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            listBoxDiceCupContent.Items.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return listBoxDiceCupContent.Items.GetEnumerator();
+        }
+
+        private void listViewDiceToChooseFrom_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            listViewDiceToChooseFrom.DoDragDrop(listViewDiceToChooseFrom.SelectedItems[0], DragDropEffects.Copy);
+        }
+
+        private void listViewDiceToChooseFrom_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(ListViewItem)))
+
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void listBoxDiceCupContent_DragDrop(object sender, DragEventArgs e)
+        {
+            ListViewItem item = e.Data.GetData(typeof(ListViewItem)) as ListViewItem;
+        }
+
+        private void listBoxDiceCupContent_DragEnter(object sender, DragEventArgs e)
+        {
+            ListViewItem item = e.Data.GetData(typeof(ListViewItem)) as ListViewItem;
+        }
+
+        private void listBoxDiceCupContent_DragOver(object sender, DragEventArgs e)
+        {
+            ListViewItem item = e.Data.GetData(typeof(ListViewItem)) as ListViewItem;
         }
     }
 }
